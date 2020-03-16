eec0a0ec9134e63ebf16c45555e1c72cd54467de
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-16 11:49:33 -0400

    finished step one and started step two

---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,12 @@
+const reducer = (state, action) =>{
+    return state;
+}
+
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 0
+}
+
+export default reducer
+export default initialState
